ea8bc550dbfd0474ebcac7854529d592516f214a
stepan.krousky <stepan.krousky@seznam.cz>
2023-11-30 10:39:59 +0100

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,20 @@
 package com.company;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
         File f = new File("textik.txt");
-        
+        try {
+            if (f.createNewFile()) {
+                System.out.println("Soubor se uspesne vytvoril");
+            } else {
+                System.out.println("Soubor se nevytvoril");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
